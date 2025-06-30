87db3a9d1ac3f919e5f9af0e2b9d30d3eca01f28
BuckyESQ <117672222+BuckyESQ@users.noreply.github.com>
2025-06-30 18:49:48 +0000

    Add API test functionality to fetch user details with authorization token

--- api/test.js
+++ api/test.js
@@ -6,4 +6,14 @@ module.exports = (req, res) => {
     method: req.method,
     headers: req.headers
   });
+
+  // Test with your actual token
+  fetch('https://api.zedchampions.com/v1/me', {
+    headers: {
+      'Authorization': 'Bearer your-token-here'
+    }
+  })
+  .then(r => r.json())
+  .then(console.log)
+  .catch(console.error);
 };
