8c97526c718b58505d90c1d78544c48297a42d19
BuckyESQ <117672222+BuckyESQ@users.noreply.github.com>
2025-06-30 20:25:48 +0000

    Update package.json format and clean up index.js comments

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
-  // This is just a placeholder - we'll redirect to our static HTML
   return null;
 }
 
-// Redirect to our static HTML file
 export async function getServerSideProps({ res }) {
   if (res) {
     res.setHeader("Location", "/index.html");
